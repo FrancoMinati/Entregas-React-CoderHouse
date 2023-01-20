451e9ba7d594f726944b38a59f692ddcffe9a301
Franco Minati <francominati2@gmail.com>
2023-01-19 21:55:59 -0300

    subiendo a vercel

--- my_app/src/App.jsx
+++ my_app/src/App.jsx
@@ -1,32 +1,11 @@
 
-import { useEffect, useState } from 'react';
 import './App.css'
-import Card from './components/Card/Card';
 import products from '../public/products.json';
-import axios from "axios";
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Navbar from './components/Navbar/NavBar';
 import { Route, Routes } from 'react-router-dom';
-import ProductCards from './components/Card/ProductCard';
 import ItemDetail from './ItemDetail';
 function App() {
-/*const [products,setProducts]=useState([]);
-const  [categories,setCategories]=useState([]);
-const urlProducts="https://fakestoreapi.com/products"
-const getProductos=()=>{
-  axios
-  .get(urlProducts)
-  .then((response)=>{setProducts(response.data)});
-}
-const getCategories=()=>{
-  axios
-  .get("https://fakestoreapi.com/products/categories")
-  .then((response)=>{setCategories(response.data)});
-}
-useEffect(() => {
-  getProductos();
-  getCategories();
-}, []);*/
 
   return (
     
